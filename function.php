2b48101e77417e8ae71615ede6ff016df7617be0
Arwildo <arwildo@gmail.com>
2023-10-25 12:20:09 +0900

    menambahkan inputan nama, unit, email dan menghubungkannya ke database

--- function.php
+++ function.php
@@ -1,13 +1,24 @@
 <?php
-    $conn = mysqli_connect("localhost", "root", "", "webcam");
+$conn = mysqli_connect("localhost", "root", "", "webcam");
 
-    if(isset($_FILES["webcam"]["tmp_name"])){
+if (isset($_GET['name']) && isset($_GET['unit']) && isset($_GET['email'])) {
+    $name = $_GET['name'];
+    $unit = $_GET['unit'];
+    $email = $_GET['email'];
+
+    if (isset($_FILES["webcam"])) {
         $tmpName = $_FILES["webcam"]["tmp_name"];
         $imageName = date("Ymd") . "-" . date("His") . '.jpeg';
         move_uploaded_file($tmpName, 'img/' . $imageName);
 
         $date = date("Y/m/d") . "-" . date("H:i:s");
-        $query = "INSERT INTO tb_image VALUES('', '$date', '$imageName')";
-        mysqli_query($conn, $query);
+        $query = "INSERT INTO tb_image (date, image, name, unit, email) VALUES (?, ?, ?, ?, ?)";
+        $stmt = mysqli_prepare($conn, $query);
+
+        if ($stmt) {
+            mysqli_stmt_bind_param($stmt, "sssss", $date, $imageName, $name, $unit, $email);
+            mysqli_stmt_execute($stmt);
+        }
     }
+}
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    require'function.php';   
+    require 'function.php';
 ?>
 
 <!DOCTYPE html>
@@ -12,100 +12,90 @@
 
     <body onload="configure();">
 
-        <div class="container">
-            <div class="smallContain">
-
-                <div id="my_camera">
-                </div>
-                <div id="results" style="display: none;position: absolute;">
-                </div>
-
-                <form class="form" method="post">
-                    <div class="form-group">
-                        <input type="text" id="name" name="name" class="input" placeholder="Nama">
-                    </div>
-
-                    <div class="form-group">
-                        <input type="text" id="unit" name="unit" class="input" placeholder="Unit">
-                    </div>
-
-                    <div class="form-group">
-                        <input type="email" id="email" name="email" class="input" placeholder="Alamat Email">
-                    </div>
-                    <button class="btn" type="submit" onClick="saveSnap();">Save</button>
-                </form>
-
-                <br>
-                <br>
+    <div class="container">
+        <div class="smallContain">
+            <div id="my_camera"></div>
+            <div id="results" style="display: none; position: absolute;">
+                <img id="webcamImage">
             </div>
+            <form class="form" method="POST">
+                <div class="form-group">
+                    <input type="text" id="name" name="name" class="input" placeholder="Nama">
+                </div>
+                <div class="form-group">
+                    <input type="text" id="unit" name="unit" class="input" placeholder="Unit">
+                </div>
+                <div class="form-group">
+                    <input type="email" id="email" name="email" class="input" placeholder="Alamat Email">
+                </div>
+                <button class="btn" type="button" onclick="saveSnap();">Save</button>
+            </form>
+            <br>
+            <br>
         </div>
+    </div>
 
-
-        <div class="absensi">
-
-            <div class="smallContain">
-                <table class="tablist">
+    <div class="absensi">
+        <div class="smallContain">
+            <table class="tablist">
+                <tr>
+                    <td>No</td>
+                    <td>Waktu</td>
+                    <td>Nama</td>
+                    <td>Unit</td>
+                    <td>Email</td>
+                    <td>Absensi</td>
+                </tr>
+                <?php
+                    $i = 1;
+                    $rows = mysqli_query($conn, "SELECT * FROM tb_image ORDER BY id DESC");
+                ?>
+                <?php foreach ($rows as $row) : ?>
                     <tr>
-                        <td>No</td>
-                        <td>Waktu</td>
-                        <td>Absensi</td>
+                        <td><?php echo $i++; ?></td>
+                        <td><?php echo $row["date"]; ?></td>
+                        <td><?php echo $row["name"]; ?></td>
+                        <td><?php echo $row["unit"]; ?></td>
+                        <td><?php echo $row["email"]; ?></td>
+                        <td><img src="img/<?php echo $row["image"]; ?>" width=200 title="<?php echo $row["image"]; ?>"></td>
                     </tr>
-                    <?php
-                        $i = 1;
-                        $rows = mysqli_query($conn, "SELECT * FROM tb_image ORDER BY id DESC");
-                    ?>
-                    <?php foreach($rows as $row) : ?>
-                        <tr>
-                            <td><?php echo $i++; ?></td>
-                            <td><?php echo $row["date"]; ?></td>
-                            <td><img src="img/<?php echo $row["image"]; ?>" width=200 title="<?php echo $row["image"]; ?>"></td>
-                        </tr>
-                    <?php endforeach; ?>
-                </table>
-            </div>
-
+                <?php endforeach; ?>
+            </table>
         </div>
+    </div>
 
-        <script type="text/javascript" src="assets/webcam.min.js"></script>
-        <script type="text/javascript">
-            function configure() {
-                Webcam.set({
-                    width: 560,
-                    height: 420,
-                    image_format: "jpeg",
-                    jpeg_quality: 90
-                });
+    <script type="text/javascript" src="assets/webcam.min.js"></script>
+    <script type="text/javascript">
+        function configure() {
+            Webcam.set({
+                width: 560,
+                height: 420,
+                image_format: "jpeg",
+                jpeg_quality: 90
+            });
+            Webcam.attach("#my_camera");
+        }
 
-                Webcam.attach("#my_camera");
-            }
+        function saveSnap() {
+            var name = document.getElementById("name").value;
+            var unit = document.getElementById("unit").value;
+            var email = document.getElementById("email").value;
 
-            function saveSnap(){
-
-                // Capture user input
-                var name = document.getElementById("name").value;
-                var unit = document.getElementById("unit").value;
-                var email = document.getElementById("email").value;
-
-                // Build the URL with parameters
-                var url =  '?name=' + encodeURIComponent(name) + 
+            var url =  '?name=' + encodeURIComponent(name) + 
                         '&unit=' + encodeURIComponent(unit) + 
                         '&email=' + encodeURIComponent(email);
 
+            Webcam.snap(function(data_uri) {
+                document.getElementById("results").style.display = "block";
+                document.getElementById("webcamImage").src = data_uri;
+            });
 
-                // Save image
-                Webcam.snap(function(data_uri){
-                    document.getElementById("results").innerHTML =
-                        '<img id="webcam" src="' + data_uri + '">';
-                });
-
-                var base64image = document.getElementById("webcam").src;
-
-                Webcam.upload(base64image,"function.php",function(code,text){
-                    alert("Kehadiran Berhasil Disimpan ✔️");
-                    // Redirect to the URL
-                    window.location.href = url;
-                });
-            }
-        </script>
+            var base64image = document.getElementById("webcamImage").src;
+            Webcam.upload(base64image, "function.php" + url, function (code, text) {
+                alert("Kehadiran Berhasil Disimpan ✔️");
+                window.location.href = url;
+            });
+        }
+    </script>
     </body>
 </html>
